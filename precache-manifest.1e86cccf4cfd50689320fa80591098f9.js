5b8df0765e3261c0d3e244b838303538440e53c0
Iris <Iris.TW.Lau@pccw.com>
2020-09-29 20:11:21 +0800

    Updates

---
+++ precache-manifest.1e86cccf4cfd50689320fa80591098f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6123b9ca1f6b7617277b0988d02937ee",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "857987984cbf0f4d0bab",
+    "url": "/simple-todo-app/static/css/main.10dedb2b.chunk.css"
+  },
+  {
+    "revision": "4dd3a872029e2042b12a",
+    "url": "/simple-todo-app/static/js/2.db6b3004.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.db6b3004.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "857987984cbf0f4d0bab",
+    "url": "/simple-todo-app/static/js/main.9913b732.chunk.js"
+  },
+  {
+    "revision": "854a73988d019f913f52",
+    "url": "/simple-todo-app/static/js/runtime-main.01792ca7.js"
+  }
+]);
